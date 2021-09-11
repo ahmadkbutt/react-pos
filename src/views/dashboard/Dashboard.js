0b804a26570daf09e58e455386b568d1e3c1f383
Ahmad Butt <87975447+ahmadkbutt@users.noreply.github.com>
2021-09-12 03:15:19 +0500

    styled

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Card, CardBody } from 'reactstrap';
 
 const Dashboard = () => {
   return (
     <>
-     Welcome 
+      <Card style={{ backgroundImage: 'radial-gradient( circle farthest-corner at 10% 20%,  rgba(97,186,255,1) 0%, rgba(166,239,253,1) 90.1% )' }}>
+        <CardBody className='text-center'>
+          <h3>Welcome to P.O and Inventory Management</h3>
+        </CardBody>
+      </Card>
     </>
   )
 }
