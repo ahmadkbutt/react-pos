874721621097598ec859ddd5bfc5f0a57217eac1
Ahmad Butt <87975447+ahmadkbutt@users.noreply.github.com>
2021-09-12 18:34:08 +0500

    logout added

--- src/containers/TheHeaderDropdown.js
+++ src/containers/TheHeaderDropdown.js
@@ -7,8 +7,15 @@ import {
   CImg
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
+import { useHistory } from 'react-router-dom'
 
 const TheHeaderDropdown = () => {
+  const history = useHistory();
+
+  const logout = () => {
+    localStorage.removeItem('authToken');
+    history.push('/')
+  }
   return (
     <CDropdown
       inNav
@@ -18,27 +25,15 @@ const TheHeaderDropdown = () => {
       <CDropdownToggle className="c-header-nav-link" caret={false}>
         <div className="c-avatar">
           <CImg
-            src={'avatars/6.jpg'}
+            src={'avatars/1.jpg'}
             className="c-avatar-img"
             alt="admin@bootstrapmaster.com"
           />
         </div>
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
-        <CDropdownItem
-          header
-          tag="div"
-          color="light"
-          className="text-center"
-        >
-          <strong>Settings</strong>
-        </CDropdownItem>
-        <CDropdownItem>
-          <CIcon name="cil-user" className="mfe-2" />Profile
-        </CDropdownItem>
-        <CDropdownItem>
-          <CIcon name="cil-settings" className="mfe-2" />
-          Settings
+        <CDropdownItem onClick = {logout}>
+          <CIcon name="cil-user" className="mfe-2" />Logout
         </CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
